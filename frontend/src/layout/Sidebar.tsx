58e083becd4e51b375edb3cb62c0ee77d92ae89d
IvsKevinSchool <ivskevin.yt@gmail.com>
2025-06-15 17:41:09 -0700

    Create Sidebar.tsx

---
+++ frontend/src/layout/Sidebar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <div className="bg-gray-800 text-white w-40 text-center items-center flex justify-center">
+      Sidebar
+    </div>
+  );
+};
+
+export default Sidebar;
