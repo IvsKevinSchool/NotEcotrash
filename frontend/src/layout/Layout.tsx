14281bdac4b046b538eacd3acaafe1dfdc6866bd
IvsKevinSchool <ivskevin.yt@gmail.com>
2025-06-15 17:41:12 -0700

    Update Layout.tsx

--- frontend/src/layout/Layout.tsx
+++ frontend/src/layout/Layout.tsx
@@ -1,11 +1,20 @@
-import React, { useState } from "react";
+import Sidebar from './Sidebar';
+import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
-    return (
-        <div>
-            Hola
-        </div>
-    );
-}
+  return (
+    <div className="flex w-full h-screen">
+      {/* Sidebar con estado de colapso */}
+      <Sidebar />
+
+      <div className="flex-1 flex flex-col w-[80%] bg-gray-300">
+        {/* Header con botón para colapsar el sidebar */}
+        <main className="flex-1 p-4 overflow-auto">
+          <Outlet /> {/* Aquí se renderizan las páginas */}
+        </main>
+      </div>
+    </div>
+  );
+};
 
 export default Layout;
