dfaace668ddecb06e9ca716242bd9a8c5eff2457
IvsKevinSchool <ivskevin.yt@gmail.com>
2025-06-15 17:35:37 -0700

    Update routes.tsx

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -2,24 +2,27 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
 // Layout
 import Layout from './layout/Layout';
+import Home from './layout/Home';
 
 // Auth components
 import Login from './auth/Login';
 import Register from './auth/Register';
 
 // View Components
-import Index from './modules/admin/index';
+import AdminRoutes from './routes/AdminRoutes';
 
 const AppRoutes = () => (
     <Router>
         <Routes>
             {/* Public routes */}
-            <Route index element={<Index />} />
+            <Route index element={<Home />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            
 
             {/* Protected routes */}
             <Route element={<Layout />}>
-                {/* <Route path="quotation/*" element={<QuotationRoutes />} /> */}
+                <Route path="/admin/*" element={<AdminRoutes />} />
             </Route>
         </Routes>
     </Router>
