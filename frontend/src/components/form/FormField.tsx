5ef31c26e586535c69813e98f5d3bb6ccc22751c
IvsKevinSchool <ivskevin.yt@gmail.com>
2025-06-20 18:34:02 -0700

    Update FormField.tsx

--- frontend/src/components/form/FormField.tsx
+++ frontend/src/components/form/FormField.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-type FormFieldProps = {
+export interface FormFieldProps extends React.InputHTMLAttributes<HTMLInputElement> {
     label: string;
     id: string;
     name: string;
@@ -9,28 +9,37 @@ type FormFieldProps = {
     required?: boolean;
     className?: string;
     error?: string;
-
-    ref?: React.Ref<HTMLInputElement>;
-    onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
-    onBlur?: (event: React.FocusEvent<HTMLInputElement>) => void;
 }
 
-const FormField: React.FC<FormFieldProps> = ({ label, id, name, type, placeholder, required, className, error, ref, onChange, onBlur }) => {
-    const labelClass = "block mb-2 text-sm font-medium text-gray-900 dark:text-white";
-    const inputClass = " bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm" +
-        " rounded-lg focus:ring-blue-600 focus:border-blue-600 block" +
-        " w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white" +
-        " dark:focus:ring-blue-500 dark:focus:border-blue-500";
+const FormField = React.forwardRef<HTMLInputElement, FormFieldProps>(
+    ({ label, id, name, type, placeholder, required, className, error, ...rest }, ref) => {
+        const labelClass = "block mb-2 text-sm font-medium dark:text-white";
+        // const inputClass = " bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm" +
+        //     " rounded-lg focus:ring-blue-600 focus:border-blue-600 block" +
+        //     " w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white" +
+        //     " dark:focus:ring-blue-500 dark:focus:border-blue-500";
+        // const labelClass = ''
+        const inputClass = 'bg-gray-100 border border-gray-300 text-gray-900 sm:text-sm p-2'
 
-    return (
-        <div>
-            <label htmlFor={label} className={labelClass}>{label}</label>
-            <input type={type} id={id} name={name} className={inputClass + className} placeholder={placeholder} required={required}
-                ref={ref} onChange={onChange} onBlur={onBlur} //  Add the ref, onChange, and onBlur props for react-hook-form
-            />
-            {error && <p className="mt-1 text-xs text-red-500">{error}</p>}
-        </div>
-    );
-}
+        return (
+            <div className="flex flex-col">
+                <div className="flex flex-col ">
+                    <label htmlFor={label} className={labelClass}>
+                        {label}
+                    </label>
+
+                    <input
+                        id={id}
+                        name={name}
+                        ref={ref}
+                        className={inputClass}
+                        type={type}
+                        {...rest}
+                    />
+                </div>
+                {error && <p className="mt-1 text-xs text-red-500">{error}</p>}
+            </div>
+        );
+    })
 
 export default FormField
