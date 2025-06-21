be9658272156f3aa9fa719acff4106850b8c5920
IvsKevinSchool <ivskevin.yt@gmail.com>
2025-06-20 19:39:32 -0700

    Update userService.ts

--- frontend/src/modules/admin/services/userService.ts
+++ frontend/src/modules/admin/services/userService.ts
@@ -3,13 +3,13 @@ import api from '../../../api';
 import type { UserFormType } from '../schemas/userSchema';
 import { userSchema } from '../schemas/userSchema';
 
-const context = 'register/'
+const context = 'accounts/auth'
 
 export const registerUser = async (data: UserFormType): Promise<UserFormType> => {
     // Validate and transform the data according to the schema
     const parsedData = userSchema.parse(data);
 
     // Make the POST request using the validated data
-    const response = await api.post<UserFormType>(context, parsedData);
+    const response = await api.post<UserFormType>(`${context}/register/`, parsedData);
     return response.data;
 }
