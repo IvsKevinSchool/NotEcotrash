bb96aaab0ab615e82b27eb7fa7008f2f35d47f09
IvsKevinSchool <ivskevin.yt@gmail.com>
2025-06-20 18:34:17 -0700

    Update Layout.tsx

--- frontend/src/layout/Layout.tsx
+++ frontend/src/layout/Layout.tsx
@@ -7,7 +7,7 @@ const Layout = () => {
       {/* Sidebar con estado de colapso */}
       <Sidebar />
 
-      <div className="flex-1 flex flex-col w-[80%] bg-gray-300">
+      <div className="flex-1 flex flex-col w-[80%] bg-gray-900">
         {/* Header con botón para colapsar el sidebar */}
         <main className="flex-1 p-4 overflow-auto">
           <Outlet /> {/* Aquí se renderizan las páginas */}
