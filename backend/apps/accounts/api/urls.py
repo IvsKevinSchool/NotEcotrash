08195b43dbc3ac154dddc790098b479a3acb6b87
IvsKevinSchool <ivskevin.yt@gmail.com>
2025-07-06 14:55:07 -0700

    Update urls.py

--- backend/apps/accounts/api/urls.py
+++ backend/apps/accounts/api/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import RegisterUserView, VerifyUserEmail, LoginUserView, TestAuthenticatedView
+from .views import RegisterUserView, VerifyUserEmail, LoginUserView
 from .views import PasswordResetView, PasswordResetConfirmView, SetNewPasswordView, LogoutUserView
 # Rutas para vistas basadas en clases
 urlpatterns = [
     path('register/', RegisterUserView.as_view(), name='user-register'),
     path('verify-email/', VerifyUserEmail.as_view(), name='user-verify-email'),
     path('login/', LoginUserView.as_view(), name='user-login'),
-    path('test-authenticated/', TestAuthenticatedView.as_view(), name='test-authenticated'),
+    # path('test-authenticated/', TestAuthenticatedView.as_view(), name='test-authenticated'),
     path('password-reset/', PasswordResetView.as_view(), name='password-reset'),
     path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('set-new-password/', SetNewPasswordView.as_view(), name='set-new-password'),
