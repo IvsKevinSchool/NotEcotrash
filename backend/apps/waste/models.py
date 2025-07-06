40ce3ebdff2f780246389fe456e85c2a89b74550
IvsKevinSchool <ivskevin.yt@gmail.com>
2025-07-06 14:40:38 -0700

    Waste model added: Waste

--- backend/apps/waste/models.py
+++ backend/apps/waste/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Waste(models.Model):
+    pk_waste = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255, unique=True)
+    description = models.CharField(max_length=255, blank=True, null=True)
+        
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True) 
