08a50344f0a21a63f1118dcf6cd0dcecc4047ad6
zahit18 <zahithdez@gmail.com>
2025-07-10 10:49:30 -0700

    Service representation endpoint

--- backend/apps/services/api/serializer.py
+++ backend/apps/services/api/serializer.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
 from apps.services.models import Status, TypeServices, Services
+from apps.client.api.serializer import ClientSerializer
+from apps.core.api.serializer import LocationSerializer
+from apps.waste.api.serializer import WasteSerializer, WasteSubCategorySerializer
 
 class StatusSerializer(serializers.ModelSerializer):
     class Meta:
@@ -24,3 +27,16 @@ class ServicesSerializer(serializers.ModelSerializer):
         extra_kwargs = {
             'service_number': {'validators': []},  # Allow duplicate service numbers for different clients or locations
         }
+    
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)  # Obtiene la representación por defecto
+        
+        # Anidar objetos completos de las relaciones (usando sus propios serializers)
+        representation['fk_clients'] = ClientSerializer(instance.fk_clients).data if instance.fk_clients else None
+        representation['fk_locations'] = LocationSerializer(instance.fk_locations).data if instance.fk_locations else None
+        representation['fk_status'] = StatusSerializer(instance.fk_status).data if instance.fk_status else None
+        representation['fk_type_services'] = TypeServicesSerializer(instance.fk_type_services).data if instance.fk_type_services else None
+        representation['fk_waste'] = WasteSerializer(instance.fk_waste).data if instance.fk_waste else None
+        representation['fk_waste_subcategory'] = WasteSubCategorySerializer(instance.fk_waste_subcategory).data if instance.fk_waste_subcategory else None
+        
+        return representation
