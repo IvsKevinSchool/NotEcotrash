51e237c7c41b789b78dd252003a4a16674da12ce
IvsKevinSchool <ivskevin.yt@gmail.com>
2025-07-12 22:58:08 -0700

    Update production.py

--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -2,12 +2,12 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["http://3.138.179.241"]
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
@@ -15,4 +15,3 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
 ]
-
