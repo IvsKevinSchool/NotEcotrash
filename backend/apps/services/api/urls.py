29d4ff1b1c09a6475a60cd9eb68c56ea8c76e83f
Onumonky <109591330+Onumonky@users.noreply.github.com>
2025-07-28 18:44:23 -0700

    backup

--- backend/apps/services/api/urls.py
+++ backend/apps/services/api/urls.py
@@ -1,10 +1,12 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
-from apps.services.api.views import StatusViewSet, TypeServicesViewSet, ServicesViewSet
+from apps.services.api.views import StatusViewSet, TypeServicesViewSet, ServicesViewSet, backup_database
 
 router = DefaultRouter()
-
 router.register(r'status', StatusViewSet, basename='status')
 router.register(r'typeServices', TypeServicesViewSet, basename='typeServices')
 router.register(r'services', ServicesViewSet, basename='services')
 
-urlpatterns = router.urls
+urlpatterns = router.urls + [
+    path('backup/', backup_database, name='backup-database'),
+]
