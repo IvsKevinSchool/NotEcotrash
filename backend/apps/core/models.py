9839234b6fb81f62486dd6f2caaba7fb0491007b
SisacOrrantia <0322104067@ut-tijuana.edu.mx>
2025-08-02 18:46:53 -0700

    MODELS - eliminacion del campo unique del modelo de location

--- backend/apps/core/models.py
+++ backend/apps/core/models.py
@@ -7,10 +7,10 @@ class Location(models.Model):
     This model can be extended in the future to include additional fields or methods.
     """
     pk_location = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=255, unique=True)
-    postcode = models.CharField(max_length=20, unique=True, blank=True, null=True)
-    interior_number = models.IntegerField(unique=True, blank=True, null=True)
-    exterior_number = models.IntegerField(unique=True, blank=True, null=True)
+    name = models.CharField(max_length=255, )
+    postcode = models.CharField(max_length=20, blank=True, null=True)
+    interior_number = models.IntegerField(blank=True, null=True)
+    exterior_number = models.IntegerField(blank=True, null=True)
     street_name = models.CharField(max_length=255, blank=True, null=True)
     neighborhood = models.CharField(max_length=255, blank=True, null=True)
     country = models.CharField(max_length=100, blank=True, null=True)
